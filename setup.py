7bd2c2c6fc8e23ac7028e7ba1def5bd929029a55
Elias Jaffe <elijaffe173@gmail.com>
2025-01-24 15:42:55 -0500

    add pydantic to setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     license="MIT License",
     packages=["canvasapi"],
     include_package_data=True,
-    install_requires=["arrow", "pytz", "requests"],
+    install_requires=["arrow", "pytz", "requests", "pydantic"],
     zip_safe=False,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
